510e4144f09192a36eebdcd5605bf46a85dc9627
Nagma Bardoi <nagmabardoi@gmail.com>
2019-11-01 02:21:34 -0500

    added gif animation and pause on click

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var tvShows = ["Vampire Diaries", "Grey's Anatomy", "Friends", "The Office", "Stranger Things", "Fresh off the Boat", "Scandal", 
-"The Walking Dead", "Riverdale", "The Flash", "Jane the Virgin", "Supernatural", "Orange is the New Black", "The Simpsons", "NCIS",
+var topics = ["Vampire Diaries", "Grey's Anatomy", "Friends", "The Office", "Stranger Things", "Fresh off the Boat", "Scandal", 
+"The Walking Dead", "Riverdale", "The Flash", "Jane the Virgin", "Supernatural", "Orange is the New Black", "The Simpsons", "How I Met Your Mother",
 "Lucifer", "Gotham", "Modern Family", "LOST", "The Good Place", "Empire", "The Big Bang Theory", "Suits", "Criminal Minds", "Game of Thrones"];
 
 
 function renderButtons() {
     $("#buttons-view").empty();
 
-    for (var i = 0; i < tvShows.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.addClass("series");
-        a.attr("data-name", tvShows[i]);
-        a.text(tvShows[i]);
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
         $("#buttons-view").append(a);
     }
 }
@@ -24,16 +24,21 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
+    console.log(response);
+
     var results = response.data;
 
     for (var i = 0; i < results.length; i++) {
     var showDiv = $("<div>");
-    var p = $("<p>");
-    $("p").text("Rating: " + results[i].rating)
+    var p = $("<p>").text("Rating: " + results[i].rating)
     var showImage = $("<img>");
-    showImage.attr("src", results[i].images.fixed_height.url);
-    showDiv.append(p);
+    showImage.attr("src", results[i].images.fixed_height_still.url);
+    showImage.attr("data-still", results[i].images.fixed_height_still.url);
+    showImage.attr("data-animate", results[i].images.fixed_height.url);
+    showImage.attr("data-state", "still");
+    showImage.addClass("gif");
     showDiv.append(showImage);
+    showDiv.append(p);
     $("#series-gifs-view").prepend(showDiv);
     }
     });
@@ -42,10 +47,25 @@ $.ajax({
 $("#add-series").on("click", function(event) {
     event.preventDefault();
     var show = $("#series-input").val().trim();
-    tvShows.push(show);
+    topics.push(show);
     renderButtons();
 });
 
 $(document).on("click", ".series", displaySeriesInfo);
 
 renderButtons();
+
+$(document).on("click", ".gif", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state == "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+
+      } else if (state == "animate"){
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+
+    };
+  });
